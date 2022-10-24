3d2b52c965d28e8627a7500e861dc031dbef87a8
Renato Ganske <renato.j@lyncas.net>
2022-10-24 17:18:52 -0300

    feat: Implementadas validações de campos com Hibernate Validator(Bean Validation.

--- quarkus-social/src/main/java/io/github/renatoganske/quarkussocial/rest/dto/CreateUserRequest.java
+++ quarkus-social/src/main/java/io/github/renatoganske/quarkussocial/rest/dto/CreateUserRequest.java
@@ -3,10 +3,16 @@ package io.github.renatoganske.quarkussocial.rest.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
+
 public class CreateUserRequest {
+    @NotBlank(message = "Nome is required.")
     private String name;
+    @NotNull(message = "Age is required.")
     private Integer age;
 
 }
