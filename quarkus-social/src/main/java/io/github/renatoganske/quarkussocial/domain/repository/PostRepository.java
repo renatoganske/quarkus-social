2c61fd46cdd0431b4a0d99e1f21e2a7865699639
Renato Ganske <renato.j@lyncas.net>
2022-10-27 16:41:31 -0300

    feat: Criada a API de Posts e implementado o método para realização de postagens.

---
+++ quarkus-social/src/main/java/io/github/renatoganske/quarkussocial/domain/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package io.github.renatoganske.quarkussocial.domain.repository;
+
+import io.github.renatoganske.quarkussocial.domain.model.Post;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PostRepository implements PanacheRepository<Post> {
+}
