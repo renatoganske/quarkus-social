676546fc82267899453cf946feb531e7ce5ad1f2
Renato Ganske Junior <renatoganske.jr@gmail.com>
2022-11-05 23:33:04 -0300

    test: implementado teste ordenado.

--- quarkus-social/src/test/java/io/github/renatoganske/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/renatoganske/quarkussocial/rest/UserResourceTest.java
@@ -5,7 +5,6 @@ import io.github.renatoganske.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 
@@ -14,13 +13,19 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
 
     @Test
     @DisplayName("should create an user successfully")
+    @Order(1)
     public void createUserTest(){
         var user = new CreateUserRequest();
         user.setName("Fulano");
@@ -31,7 +36,7 @@ class UserResourceTest {
                         .contentType(ContentType.JSON)
                         .body(user)
                         .when()
-                        .post("/users")
+                        .post(apiURL)
                         .then()
                         .extract().response();
 
@@ -41,6 +46,7 @@ class UserResourceTest {
 
     @Test
     @DisplayName("should return error when json is not valid")
+    @Order(2)
     public void createUserValidationErrorTest(){
         var user = new CreateUserRequest();
         user.setName(null);
@@ -50,7 +56,7 @@ class UserResourceTest {
                 .contentType(ContentType.JSON)
                 .body(user)
                 .when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract().response();
 
@@ -62,6 +68,19 @@ class UserResourceTest {
         assertNotNull(errors.get(1).get("message"));
 //        assertEquals("Age is required.", errors.get(0).get("message"));
 //        assertEquals("Name is required.", errors.get(1).get("message"));
+    }
+
+    @Test
+    @DisplayName("should list all users")
+    @Order(3)
+    public void listAllUsersTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(apiURL)
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.is(1));
     }
 
 }
